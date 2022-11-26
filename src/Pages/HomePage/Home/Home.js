2fb6e1f43a2bcef5a3c2abaa3f3bea5a1707ca85
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2022-11-26 10:05:26 +0600

    statistics section added

--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Statistics from '../Statistics/Statistics';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Statistics></Statistics>
         </div>
     );
 };
