1c366405c2cce0636b2fc3f044c96ae65b589848
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2022-11-28 23:50:22 +0600

    loading fix

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-sky-400"></div>
+        <div className='flex justify-center items-center'>
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-sky-400"></div>
+        </div>
     );
 };
 
